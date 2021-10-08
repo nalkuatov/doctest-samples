8e11de1610730e0c5d58dd6e6454a5998bd1b746
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2021-10-09 00:12:15 +0600

    Add one more `doctest`

--- src/Sample.hs
+++ src/Sample.hs
@@ -1,8 +1,21 @@
 module Sample where
 
+import Data.Aeson
+
 -- |
 -- >>> acc 5
 -- 6
 --
 acc :: Int -> Int
 acc = (1 +)
+
+-- |
+--
+-- Check if @doctest@ can load the deps from
+-- __package.yaml__
+--
+-- >>> :t aesonTypes
+-- ... :: Value
+--
+aesonTypes :: Value
+aesonTypes = Bool True
