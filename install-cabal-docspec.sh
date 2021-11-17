d6186fab93225d20c07080cd80df2da373a95bd4
Nurlan Alkuatov <alkuatovnurlan@gmail.com>
2021-11-17 22:51:58 +0600

    Update the installation script

--- install-cabal-docspec.sh
+++ install-cabal-docspec.sh
@@ -1,7 +1,6 @@
-git clone https://github.com/phadej/cabal-extras
-cd cabal-extras
-grep -v with-compiler cabal.project > cabal.project.out
-mv cabal.project.out cabal.project
-cd cabal-docspec
-cabal install cabal-docspec
-rm -rf cabal-extras
+echo "installing cabal-docspec"
+curl -sL https://github.com/phadej/cabal-extras/releases/download/cabal-docspec-0.0.0.20211114/cabal-docspec-0.0.0.20211114.xz > cabal-docspec.xz
+xz -d < cabal-docspec.xz > "$HOME"/.cabal/bin/cabal-docspec
+rm -f cabal-docspec.xz
+chmod a+x "$HOME"/.cabal/bin/cabal-docspec
+cabal-docspec --version
